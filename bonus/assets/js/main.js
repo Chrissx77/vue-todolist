dc08535159e9f09c0027a072113f507f9f2a6566
ChrisGarozzo <garozzochristian@gmail.com>
2023-12-12 16:17:36 +0100

    added bonus click-enter & logic(true-false)

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -31,6 +31,18 @@ createApp({
 
         addTodo() {
             this.todos.unshift({ text: this.message, done: false });
+            this.message = "";
+        },
+
+        checkDone(index) {
+            if (this.todos.length > 0) {
+                if (this.todos[index].done === false) {
+                    this.todos[index].done = true;
+                }
+                else {
+                    this.todos[index].done = false;
+                }
+            }
         }
     },
 
