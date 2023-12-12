463429ca410de236d908e6082beca4aae78a8787
ChrisGarozzo <garozzochristian@gmail.com>
2023-12-12 15:54:56 +0100

    added list todo, logic-ADD from input & logic-Remove on Vuejs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,39 @@ const { createApp } = Vue
 
 createApp({
     data() {
-       return{
-        todos: [
-            {
-                text: 'Fare i compiti',
-                done: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: true
-            },
-            {
-                text: 'Fare il bucato',
-                done: false
-            }
-        ]
-       }
+        return {
+            message: "",
+
+
+
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
     },
 
-    mounted(){
+    methods: {
+        removeTodo(index) {
+            this.todos.splice(index, 1);
+        },
+
+        addTodo() {
+            this.todos.unshift({ text: this.message, done: false });
+        }
+    },
+
+    mounted() {
         console.log("App Caricata!");
     }
 
